33c74748a8493262a398ffb5d081f4b272b9380f
anushkajlr <79107280+anushkajlr@users.noreply.github.com>
2021-03-13 11:55:43 +0530

    asks if user wants to play again

--- main.py
+++ main.py
@@ -1,43 +1,60 @@
-from board import dropCoin
-from disp import printNice
-from checkb import callCheck
 
-board = [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]] 
-counter = 1
+def main():
 
-player1, player2 = 1, 2
-while counter < 42:
+    from board import dropCoin
+    from disp import printNice
+    from checkb import callCheck
+    
+    board = [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]] 
+    counter = 1
 
-    if counter % 2 == 1:
-        #player1
-        print("\n\n------------------------------\nPlayer 1's Turn!\n")
-        printNice(board)
-        c = int(input("Enter column to drop coin: "))
-        boardx = dropCoin(board, c, player1)
-        if boardx != None:
-            printNice(boardx)
-            board = boardx
-            if callCheck(board) != None:
-                print("Player %d is the winner!" % callCheck(board))
-                break
-        else:
-            print("Try again!")
-            counter -= 1
-        counter += 1
+    player1, player2 = 1, 2
+    while counter < 42:
 
-    if counter % 2 == 0:
-        #player2
-        print("\n\n------------------------------\nPlayer 2's Turn!\n")
-        printNice(board)
-        c = int(input("Enter column to drop coin: "))
-        boardx = dropCoin(board, c, player2)
-        if boardx != None:
-            printNice(boardx)
-            board = boardx
-            if callCheck(board) != None:
-                print("Player %d is the winner!" % callCheck(board))
-                break
-        else:
-            print("Try again!")
-            counter -= 1
-        counter += 1
+        if counter % 2 == 1:
+            #player1
+            print("\n\n------------------------------\nPlayer 1's Turn!\n")
+            printNice(board)
+            c = int(input("Enter column to drop coin: "))
+            boardx = dropCoin(board, c, player1)
+            if boardx != None:
+                printNice(boardx)
+                board = boardx
+                if callCheck(board) != None:
+                    print("Player %d is the winner!" % callCheck(board))
+                    a = input("Do you want to play again  ")
+                    if a == 'yes' or a== 'Yes' or a == "YES":
+                        main()
+                    break
+                
+                    
+                    
+                    
+            else:
+                print("Try again!")
+                counter -= 1
+            counter += 1
+
+        if counter % 2 == 0:
+            #player2
+            print("\n\n------------------------------\nPlayer 2's Turn!\n")
+            printNice(board)
+            c = int(input("Enter column to drop coin: "))
+            boardx = dropCoin(board, c, player2)
+            if boardx != None:
+                printNice(boardx)
+                board = boardx
+                if callCheck(board) != None:
+                    print("Player %d is the winner!" % callCheck(board))
+                    a = input("Do you want to play again   ")
+                    if a == 'yes' or a == 'Yes' or a == 'YES':
+                        main()
+                    break
+                    
+                   
+            else:
+                print("Try again!")
+                counter -= 1
+            counter += 1
+
+main()
